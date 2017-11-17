75b0fcce5c176494246d8bde15c3a9766c978e04
jsoto78 <jsoto@mediacentertv.com.ar>
2017-11-17 13:56:39 -0300

    Update BootLauncher.java

--- src/android/BootLauncher.java
+++ src/android/BootLauncher.java
@@ -8,7 +8,7 @@ public class BootLauncher extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent serviceIntent = new Intent(context, la.megatech.findu.MainActivity.class);
+        Intent serviceIntent = new Intent(context, la.megatech.findu.CordovaApp.class);
         serviceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(serviceIntent);
     }
